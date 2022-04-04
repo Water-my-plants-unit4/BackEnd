c6b7fe7aaed42f36d81f812ec2ff2df0b619eddd
Lirong Cui <lirongcoding@gmail.com>
2022-04-04 16:38:16 -0700

    1st

---
+++ data/db-config.js
@@ -0,0 +1,8 @@
+const knex = require('knex')
+
+const configurations = require('../knexfile')
+
+const env = process.env.NODE_ENV || 'development'
+
+module.exports = knex(configurations[env])
+
---
+++ data/migrations/20220404225358_user.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+      table.increments('users_id')
+      table.text('username', 128).unique().notNullable()
+      table.text('password', 128).unique().notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+};
