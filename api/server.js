11010c9d90b426c6deb35c3f816b1f398afe98b1
Lirong Cui <lirongcoding@gmail.com>
2022-04-07 22:25:56 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,11 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors')
+const helmet = require('helmet')
+const session = requrie('express-session')
+
+const usersRouter = require('../api/plants/plants-router')
+const plantsRouter = require('../api/plants/plants-router')
 
 const server = express();
 
+server.use(helmet())
+server.use(cors())
 server.use(express.json());
+server.use(session({
+  resave: false,
+  saveUniitialized: false,
+  secret: process.env.JWT_SECRET,
+}))
+
+server.use('/users', usersRouter)
+server.use('/plants', plantsRouter)
 
 server.get('/', (req, res) => {
   res.send();
 });
 
 module.exports = server;
+
+
