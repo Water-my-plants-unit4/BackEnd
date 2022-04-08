416802b189b1c06520fd35be212ff9e5ea338235
Lirong Cui <lirongcoding@gmail.com>
2022-04-08 14:50:30 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,85 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('./users-model')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    User.get()
-        .then(user => {
-            res.json(users)
-        })
-        .catch(next)
-});
-
-router.get('/:id', (req, res) => {
-    res.json(req.user)
-});
-
-router.post('/', (req, res, next) => {
-    User.insert()
-        .then(newUser => {
-            res.status(201).json(newUser)
-        })
-        .catch(next)
-});
-
-router.put('/:id', (req, res, next) => {
-    User.update(req.params.id, { name: req.name})
-    .then(() => {
-        return User.getById(req.params.id)
-    })
-    .then(user => {
-        res.json(user)
-    })
-    .catch(next)
-});
-
-router.delete('/:id', async (req, res, next) => {
+router.get('/', async(req, res, next) => {
     try {
-        await User.remove(req.params.id)
-        res.json(req.user)
+        res.json(await User.find())
+    } catch(err) {
+        next(err)
+    }
+});
+
+// router.get('/:id', (req, res) => {
+//     res.json(req.user)
+// });
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const {username, password, phoneNumber} = req.body
+        const user = await User.findBy({username}).first()
+        if (user) {
+            return res.status(409).json({
+                message: "Username is taken"
+            })
+        } else {
+            const newUser = await User.add({
+                username,
+                password: await bcrypt.hash(password, 8),
+                phoneNumber
+            })
+            res.status(201).json(newUser)
+        }
+    } catch(err) {
+        next(err)
+    }
+});
+
+router.post('/login', async(req, res, next) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findBy({username}).first()
+        if (!user) {
+            return res.status(401).json('Please enter valid username')
+        }
+        const checkPassword = await bcrypt.compare(password, user.password)
+        if (!checkPassword) {
+            return res.status(401).json({message:"Password is incorrect"})
+        }
+        const token = jwt.sign({
+            userId: user.id
+        }, process.env.JWT_SECRET)
+
+        res.status(200).json({message: `Welcome ${user.username}`, token, user
+        })
+    }   catch(err) {
+            next(err)
+    }
+})
+
+router.put('/:id', async(req, res, next) => {
+    try {
+        const { phoneNumber, password, phoneNumber} = req.body
+        const userUpdate = await User.update({ phoneNumber, password: await bcrypt.hash(password, 8)}, req.params.id)
+        res.status(200).json({ userUpdate: userUpdate, message: 'Updated!'})
+    } catch(err) {
+        next(err)
+    }
+});
+
+router.get('/lotout', async (req, res, next) => {
+    try {
+        await req.session.destroy((err) => {
+            if(err) {
+                next(err)
+            } else {
+                res.status(204).end()
+            }
+        })
     } catch(err) {
         next(err)
     }
