3b7550b760b207e11421eca7e4be53642009167a
Lirong Cui <lirongcoding@gmail.com>
2022-04-09 21:25:01 -0700

    updated readme

--- api/server.js
+++ api/server.js
@@ -5,26 +5,23 @@ const cors = require('cors')
 const helmet = require('helmet')
 const session = require('express-session')
 
-<<<<<<< HEAD
-// const usersRouter = require('../api/plants/plants-router')
-=======
-// const usersRouter = require('../api/auth/users-router')
->>>>>>> 301a37e2e51ce26f884cd5c6ac0ac910c64a268b
-// const plantsRouter = require('../api/plants/plants-router')
+const usersRouter = require('../api/auth/users-router')
+
+const plantsRouter = require('../api/plants/plants-router')
 
 const server = express();
 
-// server.use(helmet())
-// server.use(cors())
-// server.use(express.json());
-// server.use(session({
-//   resave: false,
-//   saveUniitialized: false,
-//   secret: process.env.JWT_SECRET,
-// }))
+server.use(helmet())
+server.use(cors())
+server.use(express.json());
+server.use(session({
+  resave: true,
+  saveUninitialized: false,
+  secret: process.env.JWT_SECRET,
+}))
 
-// server.use('/auth', usersRouter)
-// server.use('/plants', plantsRouter)
+server.use('/auth', usersRouter)
+server.use('/plants', plantsRouter)
 
 server.get('/', (req, res) => {
   const time = new Date()
