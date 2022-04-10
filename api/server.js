5e073636df0fcf7a9e66e7c4b167a4ba57d5b64d
Lirong Cui <lirongcoding@gmail.com>
2022-04-09 20:00:35 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -3,24 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const helmet = require('helmet')
-const session = requrie('express-session')
+const session = require('express-session')
 
-const usersRouter = require('../api/plants/plants-router')
-const plantsRouter = require('../api/plants/plants-router')
+// const usersRouter = require('../api/plants/plants-router')
+// const plantsRouter = require('../api/plants/plants-router')
 
 const server = express();
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json());
-server.use(session({
-  resave: false,
-  saveUniitialized: false,
-  secret: process.env.JWT_SECRET,
-}))
+// server.use(helmet())
+// server.use(cors())
+// server.use(express.json());
+// server.use(session({
+//   resave: false,
+//   saveUniitialized: false,
+//   secret: process.env.JWT_SECRET,
+// }))
 
-server.use('/auth', usersRouter)
-server.use('/plants', plantsRouter)
+// server.use('/auth', usersRouter)
+// server.use('/plants', plantsRouter)
 
 server.get('/', (req, res) => {
   res.send('welcome');
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js');
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
